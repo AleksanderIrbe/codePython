5f08fd258af2189d97ba4d4f3df28f8b7a1f037a
AleksanderIrbe <aleksanderirbe@gmail.com>
2020-06-13 12:12:58 +0300

    удалены лишние строки

--- download_file_from_url.py
+++ download_file_from_url.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import requests
-import xmltodict
 import os
 
 # функция проверки наличия файла путем попытки открыть его    
@@ -41,12 +40,10 @@ if ur == '0':
 else:
     url = ur
 
-
 #проверяем, создан ли уже каталог, если нет, то создаем
 if not os.path.exists(destination):
     os.makedirs(destination)
     print('make new: ' + os.path.abspath(destination))
-
 
 #из заданного каталога и имени файла в url собираем путь к файлу
 path = os.path.abspath(destination) + '/' + os.path.basename(url)
@@ -74,33 +71,3 @@ if not is_accessible(path):
     save_file(path)
 else:
     print('file in catalog')
-
-# #проверяем, скачали ли уже архив, если нет, то скачиваем
-# if not os.listdir(destination):
-#     #скачиваем и распаковываем архив
-#     r = requests.get(url)
-#     #проверяем является ли файл архивом zip, если да, то разархивируем
-#     file_extention = os.path.splitext(url)
-#     if file_extention[1] == '.zip':
-#         with r, zipfile.ZipFile(io.BytesIO(r.content)) as archive:
-#             archive.extractall(destination)
-#     #если нет, то сохраняем в каталог
-#     else:
-#         file = open('./' + destination + '/' + os.path.basename(url), 'wb')
-#         file.write(r.content)
-#         file.close()
-
-
-# fin = open('./' + destination + '/' + os.path.basename(url), 'r', encoding='utf8')
-# xml = fin.read()
-# fin.close()
-
-# yes = 0
-# no = 0
-# parsedxml = xmltodict.parse(xml)
-# for node in parsedxml['osm']['node']:
-#     if 'tag' in node:
-#         yes += 1
-#     if 'tag' not in node:
-#         no += 1
-# print(yes,no)
